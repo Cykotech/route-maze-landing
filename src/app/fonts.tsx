a94b2ab4e7a8004734b937fd901eab99af818c16
Cykotech <cykotech.development@yahoo.com>
2023-11-29 13:57:40 -0500

    Create banner

---
+++ src/app/fonts.tsx
@@ -0,0 +1,4 @@
+import { Montserrat, Roboto } from 'next/font/google'
+
+export const montserrat = Montserrat({ subsets: ['latin'], weight: ['400', '700'], variable: '--font-monserrat' })
+export const roboto = Roboto({ weight: ['400', '700'], subsets: ['latin'], variable: '--font-roboto' })
